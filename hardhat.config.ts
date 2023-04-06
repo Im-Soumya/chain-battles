bca300478eb7ad34d05cccbfe95b7d3d515350cd
Im-Soumya <aeie2019006@rcciit.org.in>
2023-04-06 17:33:34 +0530

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,66 @@
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
+import { HardhatUserConfig } from "hardhat/config"
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const POLYSCAN_API_KEY = process.env.POLYSCAN_API_KEY || ""
+
+const config: HardhatUserConfig = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        sepolia: {
+            url: MUMBAI_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 11155111,
+        },
+    },
+    etherscan: {
+        apiKey: POLYSCAN_API_KEY
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        player: {
+            default: 1,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            }
+        ],
+    },
+    mocha: {
+        timeout: 200000, // 200 seconds max for running tests
+    },
+}
+
+export default config
